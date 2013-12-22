2d6951b65552efe0521c369957be56e7d4b420ad
James Fitzpatrick <thefishlive@hotmail.com>
2013-12-22 12:59:21 +0000

    Possibly fix not working with offline servers

--- src/main/java/uk/codingbadgers/badmin/listeners/EventListener.java
+++ src/main/java/uk/codingbadgers/badmin/listeners/EventListener.java
@@ -1,5 +1,11 @@
 package uk.codingbadgers.badmin.listeners;
 
+import static uk.codingbadgers.badmin.manager.MessageHandler.noPlayer;
+
+import com.mojang.api.profiles.HttpProfileRepository;
+import com.mojang.api.profiles.Profile;
+import com.mojang.api.profiles.ProfileCriteria;
+
 import uk.codingbadgers.badmin.TimeUtils;
 import uk.codingbadgers.badmin.bAdmin;
 import uk.codingbadgers.badmin.data.BanResult;
@@ -7,6 +13,7 @@ import uk.codingbadgers.badmin.data.BanType;
 import uk.codingbadgers.badmin.manager.BanManager;
 import uk.codingbadgers.badmin.manager.MessageHandler.KickMessage;
 
+import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.event.LoginEvent;
 import net.md_5.bungee.api.plugin.Listener;
@@ -14,9 +21,29 @@ import net.md_5.bungee.event.EventHandler;
 
 public class EventListener implements Listener {
 
+	@SuppressWarnings("deprecation")
 	@EventHandler
 	public void onPlayerJoin(final LoginEvent event) {
+
 		String uuid = event.getConnection().getUUID();
+		
+		// If not in online mode look up the id ourselves, might lag but n3wton wanted it
+		if (!ProxyServer.getInstance().getConfig().isOnlineMode()) {
+			bAdmin.getInstance().getLogger().info("Looking up id for " + event.getConnection().getName());
+			HttpProfileRepository repo = new HttpProfileRepository();
+			
+			ProfileCriteria criteria = new ProfileCriteria(event.getConnection().getName(), bAdmin.MOJANG_AGENT); 
+			Profile[] results = repo.findProfilesByCriteria(criteria);
+
+			if (results.length == 0) {
+				bAdmin.getInstance().getLogger().warning(BaseComponent.toPlainText(noPlayer(event.getConnection().getName())));
+				return;
+			}
+
+			Profile profile = results[0];
+			uuid = profile.getId();
+		}
+		
 		BanManager manager = bAdmin.getInstance().getBanManager();
 		
 		System.out.println("Player " + event.getConnection().getName() + " (" + uuid + ") has logged in");
